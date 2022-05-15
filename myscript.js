3552d162cc53d6145c209c23c363ba2006fb4914
Ziad Hamdy <ziadhamdy268@gmail.com>
2022-05-15 17:11:06 +0200

    First and last touches

---
+++ myscript.js
@@ -0,0 +1,50 @@
+const CHOICES = ["rock", "paper", "scissors"]
+
+function computerPlay(){
+    return CHOICES[Math.floor(Math.random()*(2+1))];
+}
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    //console.log(playerSelection, computerSelection);
+    
+    switch(playerSelection){
+        case "rock":
+            switch(computerSelection){
+                case "rock":
+                    return "Draw!";
+                case "paper":
+                    return "You Lose! Paper beats Rock";
+                case "scissors":
+                    return "You Win! Rock beats Scissors";
+            }
+        case "paper":
+            switch(computerSelection){
+                case "rock":
+                    return "You Win! Rock beats Scissors";
+                case "paper":
+                    return "Draw!";
+                case "scissors":
+                    return "You Lose! Paper beats Rock";
+            }
+        case "scissors":
+            switch(computerSelection){
+                case "rock":
+                    return "You Lose! Paper beats Rock";
+                case "paper":
+                    return "You Win! Rock beats Scissors";
+                case "scissors":
+                    return "Draw!";
+            }
+    }
+}
+
+function game(){
+    for(let i=0; i<5; i++){
+        playerSelection = prompt("Enter your choice");
+        console.log(playRound(playerSelection, computerPlay()));
+    }
+}
+
+game();
